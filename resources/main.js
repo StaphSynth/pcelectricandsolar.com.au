137cf0a0a7c120ca130632884265952db9b8fc92
StaphSynth <StaphSynth@users.noreply.github.com>
2016-10-26 10:53:35 +1100

    commit basic site structure

---
+++ resources/main.js
@@ -0,0 +1,13 @@
+//show/hide the main menu when nav icon clicked/tapped on mobile view
+$("#navIcon").click(function(event){
+  //console.log("click!");
+  $("#mainMenu ul").slideToggle("fast","swing");
+});
+
+/*Remove inline style from #mainMenu ul if window width > 800px on window resize event.
+Prevents inline style from over-riding CSS style rules in @media query*/
+$(window).resize(function() {
+  if($(window).width() >= 800) {
+    $("#mainMenu ul").removeAttr("style");
+  }
+});//end window resize event
