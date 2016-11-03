1395a6f3029840789cc978ec744b98b66795c761
StaphSynth <StaphSynth@users.noreply.github.com>
2016-11-03 14:57:39 +1100

    final draft complete

--- resources/main.js
+++ resources/main.js
@@ -7,7 +7,7 @@ $("#navIcon").click(function(event){
 /*Remove inline style from #mainMenu ul if window width > 800px on window resize event.
 Prevents inline style from over-riding CSS style rules in @media query*/
 $(window).resize(function() {
-  if($(window).width() >= 800) {
+  if($(window).width() >= 550) {
     $("#mainMenu ul").removeAttr("style");
   }
 });//end window resize event
