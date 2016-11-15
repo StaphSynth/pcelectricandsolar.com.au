f3c7d4fcbf3ef72621abf5f7b90b4af3ac8fe1b0
StaphSynth <StaphSynth@users.noreply.github.com>
2016-11-15 16:05:33 +1100

    added mobile menu drop-down caret with directional rotation on tap

--- resources/main.js
+++ resources/main.js
@@ -1,10 +1,12 @@
 //show/hide the main menu when nav icon clicked
 $("#navIcon").click(function(event){
-  //console.log("click!");
+  //toggle the menu
   $("#mainMenu ul").slideToggle("fast","swing");
+  //flip the caret
+  $("#navIcon .fa-caret-down").toggleClass("fa-flip-vertical");
 });
 
-/*Remove inline style from #mainMenu ul if window width > 800px on window resize event.
+/*Remove inline style from #mainMenu ul if window width > 550px on window resize event.
 Prevents inline style from over-riding CSS style rules in @media query*/
 $(window).resize(function() {
   if($(window).width() >= 550) {
